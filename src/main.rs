55e5a47c04a1da60566853faf5792e9fec4f6c8d
leo <leo@leos-MacBook-Pro.local>
2023-09-13 12:36:03 +0300

    Add liquid templates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[macro_use] extern crate rocket;
+use rocket::{tokio::fs, response::content::RawHtml};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello World"
 }
 
+#[get("/hello")]
+async fn say_hello() -> RawHtml<String> {
+    let file = fs::read_to_string("./src/templates/index.liquid").await.unwrap();
+    let template = liquid::ParserBuilder::with_stdlib().build().unwrap().parse(&file).unwrap();
+    let vals = liquid::object!({"name": "leo"});
+    let output =template.render(&vals).unwrap();
+    RawHtml(output)
+}
+
 #[launch]
 async fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, say_hello])
 }
